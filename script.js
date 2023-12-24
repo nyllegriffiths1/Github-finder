e4da40411d42b48ff0b0e3d63e43ad58971b7f5f
nyllegriffiths1 <nyllegriffiths1@googlemail.com>
2023-12-24 19:53:29 +0000

    simple application fetching user data and updating it using the dom using the GitHub API

---
+++ script.js
@@ -0,0 +1,17 @@
+async function fetchGitHubProfile(){
+    const username = document.getElementById('username').ariaValueMax;
+
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const userData = await response.json();
+
+        document.getElementById('profile-image').src = userData.avatar.url;
+        document.getElementById('profile-name').innerText = `Name: ${userData.name || 'N/A'}`;
+        document.getElementById('profile-bio').innerText = `Bio: ${userData.bio || 'N/A'}`;
+        document.getElementById('profile-repos').innerText = `Public Repositories: ${userData.public.repos}`;
+
+    } catch (error) {
+        console.log('Error fetching GitHub profile:', error.message);
+        alert('Error fetching GitHub profile. Please check the username and try again.');
+    }
+}
