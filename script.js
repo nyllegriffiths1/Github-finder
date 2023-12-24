0b44c4b7a0439858e0517ba43cd4ef7769dd979a
nyllegriffiths1 <nyllegriffiths1@googlemail.com>
2023-12-24 20:06:29 +0000

    finished now trying building yourself and expanding on it

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ async function fetchGitHubProfile(){
         const response = await fetch(`https://api.github.com/users/${username}`);
         const userData = await response.json();
 
-        document.getElementById('profile-image').src = userData.avatar.url;
+        document.getElementById('profile-image').src = userData.avatar_url;
         document.getElementById('profile-name').innerText = `Name: ${userData.name || 'N/A'}`;
         document.getElementById('profile-bio').innerText = `Bio: ${userData.bio || 'N/A'}`;
-        document.getElementById('profile-repos').innerText = `Public Repositories: ${userData.public.repos}`;
+        document.getElementById('profile-repos').innerText = `Public Repositories: ${userData.public_repos}`;
 
     } catch (error) {
         console.log('Error fetching GitHub profile:', error.message);
